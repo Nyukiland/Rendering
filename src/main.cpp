c2cf3ca2173d62f5531d621ed13a60f12c25f6c4
Nyukiland <116567790+Nyukiland@users.noreply.github.com>
2025-01-23 14:52:29 +0100

    [UPD] Small House

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,25 @@ int main()
     gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
     gl::maximize_window(); // On peut la maximiser si on veut
 
+    auto const House_Mesh = gl::Mesh
+    {{
+        .vertex_buffers = {{
+            .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
+            .data   = {
+                -0.5f, -1.f, // Position2D du 1er sommet
+                +0.5f, -1.f, // Position2D du 2ème sommet
+                -0.5f, +0.f,  // Position2D du 3ème sommet
+                +0.5f, +0.f,  // Position2D du 3ème sommet
+                +0.f, +1.f, // Position2D du 1er sommet
+            },
+        }},
+        .index_buffer   = {
+            0, 1, 2,
+            1, 2, 3,
+            2, 3, 4
+        },
+    }};
+
     float t = 0;
     while (gl::window_is_open())
     {
@@ -14,5 +33,8 @@ int main()
         if (t>1) t = 0;
         glClearColor(0.f, 0.f, t, 1.f); // Choisis la couleur à utiliser. Les paramètres sont R, G, B, A avec des valeurs qui vont de 0 à 1
         glClear(GL_COLOR_BUFFER_BIT);
+
+        gl::bind_default_shader(); // On a besoin qu'un shader soit bind (i.e. "actif") avant de draw(). On en reparle dans la section d'après.
+        House_Mesh.draw();
     }
 }
