c93dab5290cc24f2dacf6bc7f17eec926483c425
Nyukiland <116567790+Nyukiland@users.noreply.github.com>
2025-01-23 16:58:34 +0100

    [UPD] 3D shape

--- src/main.cpp
+++ src/main.cpp
@@ -22,21 +22,37 @@ int main()
     auto const House_Mesh = gl::Mesh
     {{
         .vertex_buffers = {{
-            .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
+            .layout = {gl::VertexAttribute::Position3D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
             .data   = {
-                -0.25f, -0.5f, 
-                +0.25f, -0.5f, 
-                -0.25f, +0.5f,  
-                +0.25f, +0.5f,  
-                +0.f, +1.f, 
-                +0.f, -1.f
+                -0.25f, -0.5f, -0.25f, //0
+                +0.25f, -0.5f, -0.25f, //1
+                -0.25f, +0.5f, -0.25f, //2
+                +0.25f, +0.5f, -0.25f, //3
+                -0.25f, -0.5f, +0.25f, //4
+                +0.25f, -0.5f, +0.25f, //5
+                -0.25f, +0.5f, +0.25f, //6
+                +0.25f, +0.5f, +0.25f, //7
+                +0.f, +1.f, +0.f, //8
+                +0.f, -1.f, +0.f //9
             },
         }},
         .index_buffer   = {
             0, 1, 2,
             1, 2, 3,
-            2, 3, 4,
-            0, 1, 5
+            4, 5, 6,
+            5, 6, 7,
+            1, 3, 5,
+            3, 5, 7, 
+            0, 2, 4,
+            2, 4, 6,
+            0, 1, 9,
+            5, 4, 9,
+            0, 4, 9,
+            5, 1, 9,
+            2, 3, 8,
+            6, 7, 8,
+            3, 7, 8,
+            2, 6, 8
         },
     }};
 
