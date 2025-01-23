425b9ebc6c603eb2636e6033300aa7406000c206
Nyukiland <116567790+Nyukiland@users.noreply.github.com>
2025-01-23 15:00:24 +0100

    [UPD] Moving mesh

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,16 @@ int main()
     gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
     gl::maximize_window(); // On peut la maximiser si on veut
 
-    auto const House_Mesh = gl::Mesh
+    auto const shader = gl::Shader
+    {{
+    .vertex   = gl::ShaderSource::File{"res/vertex.glsl"},
+    .fragment = gl::ShaderSource::File{"res/fragment.glsl"},
+    }};
+
+    float t = 0;
+    while (gl::window_is_open())
+    {
+auto const House_Mesh = gl::Mesh
     {{
         .vertex_buffers = {{
             .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
@@ -15,7 +24,7 @@ int main()
                 +0.5f, -1.f, // Position2D du 2ème sommet
                 -0.5f, +0.f,  // Position2D du 3ème sommet
                 +0.5f, +0.f,  // Position2D du 3ème sommet
-                +0.f, +1.f, // Position2D du 1er sommet
+                +0.f, t, // Position2D du 1er sommet
             },
         }},
         .index_buffer   = {
@@ -25,9 +34,6 @@ int main()
         },
     }};
 
-    float t = 0;
-    while (gl::window_is_open())
-    {
         // Rendu à chaque frame
         t += 0.001f;
         if (t>1) t = 0;
