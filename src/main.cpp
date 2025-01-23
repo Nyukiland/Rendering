7e381fd640a05452d876d7b45756a1a1f2093599
Nyukiland <116567790+Nyukiland@users.noreply.github.com>
2025-01-23 14:25:20 +0100

    [ADD] Blink blue

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,13 @@ int main()
     gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
     gl::maximize_window(); // On peut la maximiser si on veut
 
+    float t = 0;
     while (gl::window_is_open())
     {
         // Rendu à chaque frame
+        t += 0.001f;
+        if (t>1) t = 0;
+        glClearColor(0.f, 0.f, t, 1.f); // Choisis la couleur à utiliser. Les paramètres sont R, G, B, A avec des valeurs qui vont de 0 à 1
+        glClear(GL_COLOR_BUFFER_BIT);
     }
 }
