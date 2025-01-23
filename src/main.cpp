53b5d712f8944350623dc92bb0bffaed27e81e6e
Nyukiland <116567790+Nyukiland@users.noreply.github.com>
2025-01-23 15:27:35 +0100

    [UPD] change shape

--- src/main.cpp
+++ src/main.cpp
@@ -17,17 +17,19 @@ int main()
         .vertex_buffers = {{
             .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
             .data   = {
-                -0.5f, -1.f, // Position2D du 1er sommet
-                +0.5f, -1.f, // Position2D du 2ème sommet
-                -0.5f, +0.f,  // Position2D du 3ème sommet
-                +0.5f, +0.f,  // Position2D du 3ème sommet
-                +0.f, +1.f, // Position2D du 1er sommet
+                -0.25f, -0.5f, 
+                +0.25f, -0.5f, 
+                -0.25f, +0.5f,  
+                +0.25f, +0.5f,  
+                +0.f, +1.f, 
+                +0.f, -1.f
             },
         }},
         .index_buffer   = {
             0, 1, 2,
             1, 2, 3,
-            2, 3, 4
+            2, 3, 4,
+            0, 1, 5
         },
     }};
 
