b2e9029a86fdf6d3768bda14b7abe34c37263708
Nyukiland <roman211113@gmail.com>
2025-02-21 15:56:52 +0100

    [UPD] procedural circle

--- res/fragment3D.glsl
+++ res/fragment3D.glsl
@@ -9,7 +9,6 @@ uniform sampler2D m_texture;
 void main()
 {
     //out_color = vec4(vertex_position, 1.);
-    if (uv.x < 0.5f && uv.y < 0.5f) out_color = vec4(0,0,0,1);
-    else if (uv.x > 0.5f && uv.y > 0.5f) out_color = vec4(0,0,0,1);
-    else out_color = vec4(1.0, 0.0, 0.8, 1.0);
+    if (length(vec2(0.5f,0.5f) - uv) < 0.4f) out_color = vec4(0,0,0,0);
+    else out_color = vec4(1.0);
 }
