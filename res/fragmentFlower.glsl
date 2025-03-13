3d57e8dc94c19217a196640fcd8c37d4c7d131c2
Nyukiland <roman211113@gmail.com>
2025-03-13 16:29:56 +0100

    [UPD] light color and point light color

--- res/fragmentFlower.glsl
+++ res/fragmentFlower.glsl
@@ -7,18 +7,32 @@ in vec3 normal;
 
 uniform sampler2D m_texture;
 uniform vec3 lightDir;
+uniform vec4 lightColor;
+
+uniform vec3 pointLight;
+uniform float lightIntensity;
+uniform vec4 pointLightColor;
 
 void main()
 {
     //a + (b - a) * t
     float t = normal.z * -1;
     
-    vec4 colora = vec4(0.83, 0.0, 0.69, 1.0);
-    vec4 colorb = vec4(0.27, 1.0, 0.0, 1.0);
-    vec4 colorf = colora + (colorb - colora) * t;
+    vec4 colora = vec4(0.35, 0.0, 1.0, 1.0);
+    //vec4 colorb = vec4(0.27, 1.0, 0.0, 1.0);
+    //vec4 colorf = colora + (colorb - colora) * t;
 
     float dotValue = dot(normal, lightDir);
-    dotValue = clamp(dotValue, 0.3, 1);
+    float shadowDot = clamp(dotValue, 0.3, 1);
     
-    out_color = vec4(colorf.xyz * dotValue, 1.0) + vec4(0.1,0.1,0.1,1);
+    vec4 colorToGive = vec4(colora.xyz * shadowDot, 1.0) + vec4(lightColor.xyz * clamp(dotValue, 0, 1), 1);
+    
+    vec3 fragToLight = pointLight - vertex_position;
+    float distance = fragToLight.length();
+    float inverse01 = abs(clamp(distance/lightIntensity, 0.0 ,1.0)-1);
+
+    colorToGive += pointLightColor * inverse01;
+    colorToGive = clamp(colorToGive, 0.0, 1.0);
+
+    out_color = colorToGive;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -94,7 +94,7 @@ int main()
     .fragment = gl::ShaderSource::File{"res/fragmentFlower.glsl"},
     }};
 
-    auto const Mesh = load_mesh("res/fleur.obj");
+    auto const Mesh = load_mesh("res/Chest.obj");
 
     while (gl::window_is_open())
     {
@@ -107,7 +107,11 @@ int main()
         
         shader.bind();
         shader.set_uniform("Project", glm::mat4{projection_matrix * view_matrix});
-        shader.set_uniform("lightDir", glm::normalize(glm::vec3(0,0,1)));
+        shader.set_uniform("lightDir", glm::normalize(glm::vec3(0,1,1)));
+        shader.set_uniform("lightColor", glm::vec4(1,0,0,1));
+        shader.set_uniform("pointLight", glm::vec3(2,2,-2));
+        shader.set_uniform("lightIntensity", 5.0f);
+        shader.set_uniform("pointLightColor", glm::vec4(0,1,0,1));
         Mesh.draw();
     }
 }
